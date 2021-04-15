7793d1c194e40d634bfe2a6bd94327a08e98cf65
yuriipanchyshyn <yuriipanchyshyn1@gmail.com>
2021-04-15 14:51:40 +0300

    Initial commit

---
+++ src/main/java/com/example/telegrambotspringboot/TelegramBotSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.telegrambotspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotSpringBootApplication.class, args);
+	}
+
+}
